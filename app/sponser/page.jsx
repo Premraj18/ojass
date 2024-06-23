6702c77ca492066f60c59f6531fed68ef622fb35
Premraj18 <premrajprasad18@gmail.com>
2024-06-23 17:55:53 +0530

    remove background

--- app/sponser/page.jsx
+++ app/sponser/page.jsx
@@ -11,11 +11,29 @@ const Page = () => {
                 </div>
                 <div className='titleSPonse  mt-10 flex flex-col justify-center items-center'>
                     <h3 className='text-4xl'>Associate Title Sponser</h3>
-                    <img src="/sponserimg2.png" alt="" className='w-[70%] my-10 mt-20'/>
+                    <img src="/sponserimg2.png" alt="" className='w-[60%] my-10 mt-20'/>
                 </div>
                 <div className='titleSPonse  mt-10 flex flex-col justify-center items-center'>
                     <h3 className='text-4xl'>Bevarage Sponser</h3>
-                    <img src="/sponserimg2.png" alt="" className='w-[70%] my-10 mt-20'/>
+                    <div className='flex w-full my-10 justify-center items-center gap-10'>
+                        <img src="/Sponser/bigCola.webp" alt="" className='w-40 h-36'/>
+                        <img src="/Sponser/juice.webp" alt="" className='w-40 h-40'/>
+                        <img src="/Sponser/shudhJal.webp" alt="" className='w-40 h-40'/>
+                        <img src="/Sponser/tataGlucoPlus.webp" alt="" className='w-40 h-40'/>
+                    </div>
+                </div>
+                <div className='titleSPonse  mt-10 flex flex-col justify-center items-center'>
+                    <h3 className='text-4xl'>Media Sponser</h3>
+                    <div className='flex flex-wrap w-full my-10 justify-center items-center gap-10'>
+                        <img src="/Sponser/bigCola.webp" alt="" className='w-40 h-40'/>
+                        <img src="/Sponser/juice.webp" alt="" className='w-40 h-40'/>
+                        <img src="/Sponser/shudhJal.webp" alt="" className='w-40 h-40'/>
+                        <img src="/Sponser/tataGlucoPlus.webp" alt="" className='w-40 h-40'/>
+                        <img src="/Sponser/bigCola.webp" alt="" className='w-40 h-40'/>
+                        <img src="/Sponser/juice.webp" alt="" className='w-40 h-40'/>
+                        <img src="/Sponser/shudhJal.webp" alt="" className='w-40 h-40'/>
+                        <img src="/Sponser/tataGlucoPlus.webp" alt="" className='w-40 h-40'/>
+                    </div>
                 </div>
             </div>
         </div>
