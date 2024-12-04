785361316204628b2a2815c22109942f0f3b1933
Premraj18 <premrajprasad18@gmail.com>
2024-12-04 11:50:27 +0530

    abc

--- app/event/Cardevent.jsx
+++ app/event/Cardevent.jsx
@@ -27,7 +27,7 @@ const CardEvent = ({data}) => {
                         <CarouselItem key={index} className="sm:basis-1/1 md:basis-1/3 lg:basis-1/4 flex justify-center">
                             <div className='lg:w-[22vw] w-[80vw] h-[420px] relative flex flex-col items-center justify-center bg-white/10 rounded-xl px-6 gap-y-2 border'>
                                 <div className=' w-[90%] h-[85%] mt-8'>
-                                    <div className=''>
+                                    <div className='border'>
                                         <img src={data.img} alt="" className=' h-[320px] w-full object-cover' />
                                     </div>
                                 </div>
--- components/EventCard.jsx
+++ components/EventCard.jsx
@@ -12,67 +12,67 @@ import Autoplay from "embla-carousel-autoplay";
 const Data = [
   {
     id: "1",
-    title: " ELECTRO-SPECTION",
+    title: "VISHWA CODE GENESIS",
     description: "Electrical circuits form the foundation of power generation, and in this event, participants design and construct electrical and electronic circuits swiftly and efficiently on a breadboard.",
     imgUrl: "/sponser/skoda.webp"
   },
   {
     id: "2",
-    title: "MATLINK",
+    title: "SILICON VALLEY",
     description: "Solving intricate Electrical Enginnering challenges necessitates the application of Mathematical and analytical tools, A skill set put to the test in this event that delves into the capativating realm of matlab.",
     imgUrl: "/sponser/img6.webp"
   },
   {
     id: "3",
-    title: "PROGEM",
+    title: "RISE OF MACHINES",
     description: "It is crucial to control and realize the working of industrial equipment. this event deals with PLC(Programmable Logic Controller).",
     imgUrl: "/sponser/img7.webp"
   },
   {
     id: "4",
-    title: "NAVRITI",
+    title: "AAVARTAN",
     description: "The art of paper presentation requires study, implementation and presentation of ideas. students will need to present their ideas in a scientific and research-oriented manner.",
     imgUrl: "/sponser/img8.webp"
   },
   {
     id: "5",
-    title: "I-MATTER",
+    title: "AAKRITI",
     description: "I-matter offers students a unique industrial visit, providing firsthand exposure to real world technology operations, and bridging the gap between theory and practice.",
     imgUrl: "/sponser/img9.webp"
   },
   {
     id: "6",
-    title: "ADHYAYAN",
+    title: "CIRCUIT HOUSE",
     description: "This event involves a case-study in which all major decisions related to the organisation are taken. this event will bring in great white-collar experience.",
     imgUrl: "/sponser/img10.webp"
   },
   {
     id: "7",
-    title: "PAIR-IN-THESIS",
+    title: "METAMORPHICA",
     description: "Codes are the bricks on which magnificent castles can be built. this event revolves around the interesting world of competitive programming.",
     imgUrl: "/sponser/img11.webp"
   },
   {
     id: "8",
-    title: "ELECTRO-HACK",
+    title: "ARTHSHASHTRA",
     description: "This event provides students a platform to solve some problems we face in our daily lives, and thus inculcate a culture of product innovation and problem solving",
     imgUrl: "/sponser/img11.webp"
   },
   {
     id: "9",
-    title: "ANALYTICA",
+    title: "ARMAGEDDON",
     description: "Analytica is designed to evaluate participants' analytical and problem solving skills while introducing them to the trending domains of data analytics and data science.",
     imgUrl: "/sponser/img11.webp"
   },
   {
     id: "10",
-    title: " ABHYAS",
+    title: " ROBO REALM",
     description: "Placements are of utmost importance. This event tests your overall communication, technical, and aptitude skills with a subdued experience of campus placement.",
     imgUrl: "/sponser/img11.webp"
   },
   {
     id: "11",
-    title: "QRIOUS",
+    title: "NO GROUND ZONE",
     description: "A brain-teasing challenge, qrious is a quiz based event revolving around the dynamic universe of electrical engineering and related sciences, stimulating intellectual curiosity.",
     imgUrl: "/sponser/img11.webp"
   }
