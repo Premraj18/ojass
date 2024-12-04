50e5fab557360115413fcab03918e8a5484a8ea0
Premraj18 <premrajprasad18@gmail.com>
2024-12-04 11:38:55 +0530

    abc

--- app/event/viswaCodeGenesis.jsx
+++ app/event/viswaCodeGenesis.jsx
@@ -6,8 +6,8 @@ import Data from "./event.json"
 const viswaCodeGenesis = () => {
     return (
         <>
-            <div className=" w-[95%] h-[70vh] cursor-pointer text-white p-6 rounded-[0.8rem] scale-95 bg-white/10 border-gray-200 border-[1px] shadow-md">
-                <h2 className="text-center text:3xl lg:text-5xl mb-5">Viswa Code Genesis</h2>
+            <div className=" w-[95%] h-[80vh] cursor-pointer text-white p-6 rounded-[0.8rem] scale-95 bg-white/10 border-gray-200 border-[1px] shadow-md">
+                <h2 className="text-center text-3xl lg:text-5xl mb-5">Viswa Code Genesis</h2>
                 <CardEvent data={Data[0]}/>
             </div>
         </>
