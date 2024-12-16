687d82b52dba12ec82178c8788f4434c8bc1b1aa
Premraj18 <premrajprasad18@gmail.com>
2024-12-16 19:30:29 +0530

    abcd

--- components/Footer2.jsx
+++ components/Footer2.jsx
@@ -10,25 +10,12 @@ export default function Foter() {
         <footer className="body-font " style={{ background: 'linear-gradient(rgba(0, 0, 0, 0.919),rgba(109, 109, 109, 0.133))', boxShadow: '0px 0.01px 5px rgba(24, 22, 22, 0.155)' }} >
             <div className="container px-5 pt-16 mx-auto" >
                 <div className="flex flex-wrap md:text-left text-center order-first">
-                    <div className="lg:w-1/3 md:w-1/2 w-full sm:px-4 flex flex-col justify-center items-center" >
-                        <h2 className="title-font font-medium text-white tracking-widest text-sm mb-3 lg:text-xl" >Quick Link</h2>
-                        <nav className="list-none w-full sm:w-[400px] mb-5 text-sm lg:text-base flex justify-center gap-x-1 ">
-                            <li>
-                                <Link href='/' className="text-gray-50 hover:text-red-500 cursor-pointer " >Home |</Link>
-                            </li>
-                            <li>
-                                <Link href='/event' className="text-gray-50 hover:text-red-500 cursor-pointer" >Event |</Link>
-                            </li>
-                            <li>
-                                <Link href='/ourteam' className="text-gray-50 hover:text-red-500 cursor-pointer" >Team |</Link>
-                            </li>
-                            <li>
-                                <Link href='/sponsor' className="text-gray-50 hover:text-red-500 cursor-pointer" >Sponsor |</Link>
-                            </li>
-                            <li>
-                                <Link href='/contact' className="text-gray-50 hover:text-red-500 cursor-pointer" >Contact us</Link>
-                            </li>
-                        </nav>
+                    <div className="lg:w-1/3 md:w-1/2 justify-center lg:items-start items-center lg:pl-16 w-full sm:px-4 flex flex-col" >
+                        <h2 className="title-font font-medium text-white tracking-widest mb-3 text-xl px-2" >OJASS'25</h2>
+                        <p className='text-sm w-[350px] px-2'>Ojass, NIT Jamshedpur's annual national-level Techno-Management Festival, stands as
+                            the second largest event of its kind in Eastern India. With a staggering turnout of
+                            over 8000+ footfall including students, professionals, educators, and
+                            artists from top colleges across the nation.</p>
                     </div>
                     {/* <div className="lg:w-1/4 md:w-1/2 w-full px-4 flex flex-col justify-center items-center lg:pl-20">
                         <h2 className="title-font font-medium text-white tracking-widest text-sm mb-3 uppercase lg:text-lg" >About</h2>
@@ -37,37 +24,69 @@ export default function Foter() {
                         </nav>
                     </div> */}
 
-                    <div className="lg:w-1/4 md:w-1/2 w-full px-4 flex flex-col justify-center items-center md:ml-28 ">
-                            
+                    <div className="lg:w-1/4 md:w-1/2 w-full px-4 flex flex-col justify-center items-center md:ml-12 my-5 lg:my-0 ">
+
                         <nav className="list-none mt-5">
                             <li>
-                                <p className="text-gray-50  lg:text-base text-sm text-center flex justify-center items-center" ><MdLocationPin size={35}/> <span> National Institute of Technology Jamshedpur, <br/> Adityapur, Jamshedpur, Jharkhand 831014 </span></p>
+                                <p className="text-gray-50  lg:text-base text-sm text-center flex justify-center items-center" ><MdLocationPin size={35} /> <span> National Institute of Technology Jamshedpur, <br /> Adityapur, Jamshedpur, Jharkhand 831014 </span></p>
                             </li>
                             <li>
-                                <p className="text-gray-50  lg:text-base text-sm flex gap-x-5 my-3 items-center justify-center" ><BsTelephone size={25}/><span>+91 8863832703</span></p>
+                                <p className="text-gray-50  lg:text-base text-sm flex gap-x-5 my-3 items-center justify-center" ><BsTelephone size={25} /><span>+91 8863832703</span></p>
                             </li>
                             <li>
-                                <p className="text-gray-50  lg:text-base text-sm flex gap-x-5 my-3 items-center justify-center" ><MdMail size={25}/><span>ojass@nitjsr.ac.in</span> </p>
+                                <p className="text-gray-50  lg:text-base text-sm flex gap-x-5 my-3 items-center justify-center" ><MdMail size={25} /><span>ojass@nitjsr.ac.in</span> </p>
                             </li>
                         </nav>
                     </div>
+                    <div className="lg:w-1/3 md:w-1/2 w-full sm:px-4 flex flex-col justify-center items-center" >
+                        <h2 className="title-font font-medium text-white tracking-widest mb-3 text-xl" >Quick Link</h2>
+
+                        <div className="w-full lg:w-3/4 lg:ml-16 sm:px-4 flex justify-center" >
+                            <nav className="list-none w-full mb-5 text-sm lg:text-base flex flex-col gap-1 ">
+                                <li>
+                                    <Link href='/' className="text-gray-50 hover:text-red-500 cursor-pointer " >Home</Link>
+                                </li>
+                                <li>
+                                    <Link href='/event' className="text-gray-50 hover:text-red-500 cursor-pointer" >Events</Link>
+                                </li>
+                                <li>
+                                    <Link href='/ourteam' className="text-gray-50 hover:text-red-500 cursor-pointer" >Team</Link>
+                                </li>
+
+                            </nav>
+                            <nav className="list-none w-96 mb-5 text-sm lg:text-base flex flex-col gap-2 ">
+                                <li>
+                                    <Link href='/privacypolicy' className="text-gray-50 hover:text-red-500 cursor-pointer " >Privacy Policy</Link>
+                                </li>
+                                <li>
+                                    <Link href='/termcond' className="text-gray-50 hover:text-red-500 cursor-pointer" >Term & Condition</Link>
+                                </li>
+
+                            </nav>
+                        </div>
+                    </div>
                 </div>
-                <div className='lg:text-lg text-white flex pt-5 justify-center md:pt-0 md:justify-normal'>
-                    <p >Designed&Develop By: <a href="" className='text-red-500'>Prem Raj</a> </p>
+                <div className='flex md:flex-row flex-col justify-between'>
+                    <div className='lg:text-lg text-white flex pt-5 justify-center md:pt-0 md:justify-normal'>
+                        <p >Designed&Develop By: <a href="" className='text-red-500'>Prem Raj</a> </p>
+                    </div>
+                    <div className='lg:text-lg text-white flex pt-5 justify-center md:pt-0 md:justify-normal'>
+                        <p >Visitors : <a href="" className='text-red-500'>45327</a> </p>
+                    </div>
                 </div>
             </div>
 
             {/* <div className="bg-gray-200"> */}
-                <div className="container px-5 py-3 mx-auto flex items-center justify-center sm:flex-row flex-col">
-                    <Link href={'/'} className='flex'>
-                        <div className="flex ">
-                            <h1 className=' text-2xl font-bold text-white  px-2 py-1 rounded' >OJASS</h1>
-                        </div>
-                    </Link>
-                    <p className="text-sm text-gray-50 sm:ml-6 sm:mt-0 mt-4" >© 2024 NIT Jamshedpur —
-                        <a href="https://twitter.com/knyttneve" rel="noopener noreferrer" className="text-gray-100 ml-1" target="_blank" >www.ojass.org</a>
-                    </p>
-                    {/* <span className="inline-flex sm:ml-auto mr-20 sm:mt-0 mt-4 justify-center sm:justify-start">
+            <div className="container px-5 py-3 mx-auto flex items-center justify-center sm:flex-row flex-col">
+                <Link href={'/'} className='flex'>
+                    <div className="flex ">
+                        <h1 className=' text-2xl font-bold text-white  px-2 py-1 rounded' >OJASS</h1>
+                    </div>
+                </Link>
+                <p className="text-sm text-gray-50 sm:ml-6 sm:mt-0 mt-4" >© 2024 NIT Jamshedpur —
+                    <a href="https://twitter.com/knyttneve" rel="noopener noreferrer" className="text-gray-100 ml-1" target="_blank" >www.ojass.org</a>
+                </p>
+                {/* <span className="inline-flex sm:ml-auto mr-20 sm:mt-0 mt-4 justify-center sm:justify-start">
                         <a href='https://www.facebook.com/teamdaksh.nitjsr/' target='_blank' className="text-gray-50 hover:text-red-500 cursor-pointer">
                             <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} className="w-5 h-5" viewBox="0 0 24 24">
                                 <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z" />
@@ -87,7 +106,7 @@ export default function Foter() {
                             </svg>
                         </a>
                     </span> */}
-                </div>
+            </div>
             {/* </div> */}
         </footer>
     )
--- components/Media.jsx
+++ components/Media.jsx
@@ -16,7 +16,7 @@ const Media = () => {
         }
     })
     return (
-        <motion.div className="lg:flex hidden justify-center mx-3 gap-4  flex-col w-10  items-center fixed top-[40%] left-0 z-10 text-white"
+        <motion.div className="lg:flex hidden justify-center mx-3 gap-4  flex-col w-10  items-center fixed top-[40%] left-0 z-50 text-white"
             variants={{
                 visible: { opacity: 0 },
                 hidden: { opacity:1 },
@@ -24,10 +24,10 @@ const Media = () => {
             // animate={hidden ? "hidden" : "visible"}
             transition={{ duration: 0.3, ease: "easeInOut" }}
         >
-            <a href="https://www.instagram.com/team.daksh/" target='_blank' className='hidden lg:flex  items-center justify-center w-full h-10 hover:text-pink-600'><BsInstagram size={23} /></a>
-            <a href="https://www.facebook.com/teamdaksh.nitjsr/" target='_blank' className='hidden lg:flex  items-center justify-center w-full h-10 hover:text-blue-600'><BsFacebook size={23} /></a>
-            <a href="https://in.linkedin.com/company/teamdaksh" target='_blank' className='hidden lg:flex  items-center justify-center w-full h-10 hover:text-blue-600'><BsLinkedin size={23} /></a>
-            <a href="https://www.youtube.com/@teamdaksh9836" target='_blank' className='hidden lg:flex  items-center justify-center w-full h-10 hover:text-red-600'><BsYoutube size={23} /></a>
+            <a href="https://www.instagram.com/ojass.nitjsr/" target='_blank' className='hidden lg:flex  items-center justify-center w-full h-10 hover:text-pink-600'><BsInstagram size={23} /></a>
+            <a href="https://www.facebook.com/Ojassnitjamshedpur/" target='_blank' className='hidden lg:flex  items-center justify-center w-full h-10 hover:text-blue-600'><BsFacebook size={23} /></a>
+            <a href="https://www.linkedin.com/company/ojassnitjsr/" target='_blank' className='hidden lg:flex  items-center justify-center w-full h-10 hover:text-blue-600'><BsLinkedin size={23} /></a>
+            <a href="https://www.youtube.com/@OJASS.NITJSR" target='_blank' className='hidden lg:flex  items-center justify-center w-full h-10 hover:text-red-600'><BsYoutube size={23} /></a>
         </motion.div>
     )
 }
