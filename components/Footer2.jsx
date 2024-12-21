1fcb581c77526b5f3c2c114510268b4ca0392216
Premraj18 <premrajprasad18@gmail.com>
2024-12-21 08:46:31 +0530

    ui changes

--- components/Footer2.jsx
+++ components/Footer2.jsx
@@ -3,8 +3,23 @@ import Link from 'next/link'
 import { useContext } from 'react'
 import { BsTelephone } from 'react-icons/bs'
 import { MdLocationPin, MdMail } from 'react-icons/md'
+import { useEffect, useState } from "react";
+import Cookies from "js-cookie";
 
 export default function Foter() {
+    const [refreshCount, setRefreshCount] = useState(0);
+
+    useEffect(() => {
+        // Get the current count from the cookie or initialize to 0
+        const currentCount = parseInt(Cookies.get("refreshCount") || "0", 10);
+
+        // Increment the count
+        const newCount = currentCount + 1;
+
+        // Update the state and cookie with the new count
+        setRefreshCount(newCount);
+        Cookies.set("refreshCount", newCount);
+    }, []);
 
     return (
         <footer className="body-font " style={{ background: 'linear-gradient(rgba(0, 0, 0, 0.919),rgba(109, 109, 109, 0.133))', boxShadow: '0px 0.01px 5px rgba(24, 22, 22, 0.155)' }} >
@@ -74,7 +89,7 @@ export default function Foter() {
                         <p >Designed&Develop By: <a href="" className='text-red-500'>Prem Raj</a> </p>
                     </div>
                     <div className='lg:text-lg text-white flex pt-5 justify-center md:pt-0 md:justify-normal'>
-                        <p >Visitors : <span className='text-red-500'>45327</span> </p>
+                        <p >Visitors : <span className='text-red-500'>{45362+refreshCount}</span> </p>
                     </div>
                 </div>
             </div>
--- components/ui/parallax-scroll.tsx
+++ components/ui/parallax-scroll.tsx
@@ -51,7 +51,7 @@ export const ParallaxScroll = ({
             >
               <img
                 src={el}
-                className="h-80 w-full border object-cover object-left-top rounded-lg gap-10 !m-0 !p-0"
+                className="h-80 w-full border md:object-cover object-left-top rounded-lg gap-10 !m-0 !p-0"
                 height="400"
                 width="400"
                 alt="thumbnail"
@@ -64,7 +64,7 @@ export const ParallaxScroll = ({
             <motion.div style={{ y: translateSecond }} key={"grid-2" + idx}>
               <img
                 src={el}
-                className="h-80 w-full border object-cover object-left-top rounded-lg gap-10 !m-0 !p-0"
+                className="h-80 w-full border md:object-cover object-left-top rounded-lg gap-10 !m-0 !p-0"
                 height="400"
                 width="400"
                 alt="thumbnail"
@@ -77,7 +77,7 @@ export const ParallaxScroll = ({
             <motion.div style={{ y: translateThird }} key={"grid-3" + idx}>
               <img
                 src={el}
-                className="h-80 w-full border object-cover object-left-top rounded-lg gap-10 !m-0 !p-0"
+                className="h-80 w-full border md:object-cover object-left-top rounded-lg gap-10 !m-0 !p-0"
                 height="400"
                 width="400"
                 alt="thumbnail"
@@ -86,11 +86,11 @@ export const ParallaxScroll = ({
           ))}
         </div>
       </div>
-      <div className='flex flex-col md:flex-row px-8 md:justify-evenly justify-center items-center -mt-28 mb-5'>
+      <div className='flex flex-col md:flex-row px-8 md:justify-evenly justify-center items-center -mt-28 mb-5 gap-3 '>
         <iframe className='w-[350px] md:w-[550px] h-[315px] md:h-[415px] rounded-3xl' src="https://www.youtube.com/embed/v025irxdSmk?si=QualeiZb-NDBwE_D" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
         <InstagramEmbed embedUrl="https://www.instagram.com/reel/C5m5dVUvSSA/?utm_source=ig_embed&amp;utm_campaign=loading" />
       </div>
-      <div className='flex flex-col md:flex-row px-8 md:justify-evenly justify-center items-center  mt-10 mb-5'>
+      <div className='flex flex-col md:flex-row px-8 md:justify-evenly justify-center items-center  mt-10 mb-5 gap-3'>
         <iframe className='w-[350px] md:w-[550px] h-[315px] md:h-[415px] rounded-3xl' src="https://www.youtube.com/embed/HTBQUfxH7iw?si=dMoZms4wbwb_wMpQ" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
         <InstagramEmbed embedUrl="https://www.instagram.com/reel/C5vzK4uSH9D/?utm_source=ig_embed&amp;utm_campaign=loading" />
       </div>
