e0d805dc894dcb19f8ae2e0ea0e4bfb97564b87c
ayush kumar <ayush2422005@gmail.com>
2024-12-27 21:18:57 +0530

    some minar fixes

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone',
   reactStrictMode: true,
   images: {
      remotePatterns: [
@@ -8,4 +10,6 @@ module.exports = {
       },
     ]
     },
-  };
+};
+
+module.exports = nextConfig;
