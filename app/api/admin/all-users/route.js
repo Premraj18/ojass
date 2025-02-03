2d074832db4c5f0357c33210b8a11d5d7ec4cad7
Premraj18 <premrajprasad18@gmail.com>
2025-02-03 11:25:29 +0530

    protect route

--- app/api/admin/all-users/route.js
+++ app/api/admin/all-users/route.js
@@ -1,10 +1,33 @@
 import { NextResponse } from 'next/server';
 import connectDB from '@/lib/db';
 import User from '@/models/User';
+import jwt from 'jsonwebtoken';
 
 export async function GET(req) {
   try {
     await connectDB();
+
+    const token = req.cookies.get('adminToken');
+
+    if (!token) {
+      return NextResponse.json(
+        { error: 'Unauthorized access' },
+        { status: 401 }
+      );
+    }
+
+    // console.log("token", token);
+
+    // Verify token
+    let decoded;
+    try {
+      decoded = jwt.verify(token.value, process.env.JWT_SECRET);
+    } catch (error) {
+      return NextResponse.json(
+        { error: 'Invalid or expired token' },
+        { status: 401 }
+      );
+    }
     
     const { searchParams } = new URL(req.url);
     const page = parseInt(searchParams.get('page')) || 1;
